cfdf6ba4b90d6975b6161441df1c40c794d7c177
yjshi2015 <495054021@qq.com>
2017-05-12 07:45:25 +0800

    实现展现层逻辑

--- src/main/java/com/baobaotao/web/BoardManageController.java
+++ src/main/java/com/baobaotao/web/BoardManageController.java
@@ -1,5 +1,9 @@
 package com.baobaotao.web;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +15,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.baobaotao.cons.CommonConstant;
 import com.baobaotao.dao.Page;
 import com.baobaotao.domain.Board;
+import com.baobaotao.domain.Topic;
+import com.baobaotao.domain.User;
 import com.baobaotao.service.ForumService;
 
 @Controller
@@ -32,6 +38,43 @@ public class BoardManageController extends BaseController{
 		view.addObject("board",board);
 		view.addObject("pagedTopic",pagedTopic);
 		view.setViewName("/listBoardTopics");
+		return view;
+	}
+	
+	//新增主题帖子页面
+	@RequestMapping(value="/board/addTopicPage-{boardId}",method=RequestMethod.GET)
+	public ModelAndView addTopicPage(@PathVariable Integer boardId) {
+		ModelAndView view = new ModelAndView();
+		view.addObject("boardId",boardId);
+		view.setViewName("/addTopic");
+		return view;
+	}
+	
+	//添加一个主题帖
+	@RequestMapping(value="/board/addTopic",method=RequestMethod.POST)
+	public String addTopic(HttpServletRequest request, Topic topic) {
+		User user = getSessionUser(request);
+		topic.setUser(user);
+		Date now = new Date();
+		topic.setCreateTime(now);
+		topic.setLastPost(now);
+		forumService.addTopic(topic);
+		String targetUrl = "/board/listBoardTopics-" + topic.getBoardId() + ".html";
+		return "redirect:" + targetUrl;
+	}
+	
+	//列出主题的所有帖子
+	@RequestMapping(value="/board/listTopicPosts-{topicId}",method=RequestMethod.GET)
+	public ModelAndView listTopicPosts(@PathVariable Integer topicId,
+			@RequestParam(value="pageNo",required=false) Integer pageNo) {
+		ModelAndView view = new ModelAndView();
+		Topic topic = forumService.getTopicByTopicId(topicId);
+		pageNo = pageNo==null?1:pageNo;
+		Page pagePost = forumService.getPagedPosts(topicId, 
+				pageNo, CommonConstant.PAGE_SIZE);
+		view.addObject("topic",topic);
+		view.addObject("pagePost",pagePost);
+		view.setViewName("/listTopicPosts");
 		return view;
 	}
 }
